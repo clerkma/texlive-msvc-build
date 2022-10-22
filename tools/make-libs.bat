6d9704b93d733d9d93f27446075bca3248556231
Clerk Ma <clerkma@gmail.com>
2022-10-23 01:16:01 +0800

    fixed typos.

--- tools/make-libs.bat
+++ tools/make-libs.bat
@@ -1,5 +1,5 @@
-for %%t in (cairo freetype gmp graphite2 harfbuzz icudata icuuc
-    kpathsea libgd libpaper luajit mpfr otfcc pixman png potrace pplib ptexenc runtime
+for %%t in (cairo freetype gd gmp graphite2 harfbuzz icudata icuuc
+    kpathsea luajit mpfr otfcc paper pixman png potrace pplib ptexenc runtime
     runtimep teckit texlua53 xpdf z zzip) do (
     nmake -f make\lib%%t.nmake
 )
