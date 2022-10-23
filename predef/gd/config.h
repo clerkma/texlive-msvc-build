44d7c5b56261200e22a8447b67a3ceaa9adda50f
Clerk Ma <clerkma@gmail.com>
2022-10-23 13:37:30 +0800

    fixed gd build.

---
+++ predef/gd/config.h
@@ -0,0 +1,11 @@
+/* config.h for libgd */
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define HAVE_LIBFREETYPE 1
+#define HAVE_FT2BUILD_H 1
+#define HAVE_LIBPNG 1
+#define HAVE_LIBZ 1
+#define HAVE_SYS_STAT_H 1
+
+#endif /* __CONFIG_H__ */
