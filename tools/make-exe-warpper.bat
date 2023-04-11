10aa989760ed1277d9cd883ecdbe4217e584dce4
Clerk Ma <clerkma@gmail.com>
2023-04-11 17:15:10 +0800

    updated makefile files.

--- tools/make-exe-warpper.bat
+++ tools/make-exe-warpper.bat
@@ -1,4 +1,4 @@
 FOR /r %TLWORKS% %%F IN (*.lib) DO  (
-    cl -nologo -c -O2 -DDLLPROC=dll%%~nFmain -Fo%TLBUILD%\call-%%~nF.obj %TLROOT%\texk\texlive\w32_wrapper\calldll.c
+    cl -nologo -c -O2 -DDLLPROC=dll%%~nFmain -Fo%TLBUILD%\call-%%~nF.obj %TLROOT%\texk\texlive\windows_wrapper\calldll.c
     link -nologo -out:%TLWORKS%\%%~nF.exe %TLBUILD%\call-%%~nF.obj %%F
 )
--- tools/make-libs.bat
+++ tools/make-libs.bat
@@ -1,5 +1,5 @@
 for %%t in (cairo freetype gd gmp graphite2 harfbuzz icudata icuio icui18n icuuc
     kpathsea luajit mpfr otfcc paper pixman png potrace pplib ptexenc runtime
     runtimep teckit texlua53 xpdf z zzip) do (
-    nmake -f make\lib%%t.nmake
+    nmake -nologo -f make\lib%%t.nmake
 )
