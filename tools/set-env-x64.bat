7a04881d9ac2dac310d138b1ee3fd93ad691dbff
Clerk Ma <clerkma@gmail.com>
2025-01-09 23:34:26 +0800

    updated env scripts.

--- tools/set-env-x64.bat
+++ tools/set-env-x64.bat
@@ -1,13 +1,16 @@
 @echo off
-set VS22C="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-set VS22B="C:\Program Files(x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-if exist %VS22C% (
-    call %VS22C%
+
+for /f "usebackq tokens=*" %%i in (
+`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"^
+ -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.*^
+ -property installationPath`) do (
+  set InstallDir=%%i
+)
+set VSENVBAT="%InstallDir%\VC\Auxiliary\Build\vcvars64.bat"
+if exist %VSENVBAT% (
+    call %VSENVBAT%
     exit /b 1
 )
-if exist %VS22B% (
-    call %VS22B%
-    exit /b 1
-)
+
 echo "I cannot found compiler. Please make sure that you have installed it."
 @echo on
